178fa3861aa223ccaca6e616bdd48dbbbe7ce71d
ichigotake <k.wisiiy@gmail.com>
2014-07-01 22:30:52 +0900

    Set header title on circle detail pager

--- Creation/src/main/java/circlebinder/creation/app/phone/CircleDetailPagerActivity.java
+++ Creation/src/main/java/circlebinder/creation/app/phone/CircleDetailPagerActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
+import android.view.View;
 
 import net.ichigotake.common.app.ActivityNavigation;
 import net.ichigotake.common.app.FragmentPagerAdapter;
@@ -19,31 +21,41 @@ import circlebinder.common.search.CircleSearchOption;
 import circlebinder.creation.BaseActivity;
 import circlebinder.creation.R;
 import circlebinder.creation.circle.CircleDetailFragment;
+import circlebinder.creation.circle.CircleDetailViewHolder;
+import circlebinder.creation.circle.OnCirclePageChangeListener;
 import circlebinder.creation.event.CircleTable;
 import circlebinder.creation.search.CircleCursorConverter;
 
-public final class CircleDetailPagerActivity extends BaseActivity implements Legacy {
+public final class CircleDetailPagerActivity extends BaseActivity
+        implements Legacy, OnCirclePageChangeListener {
 
     public static final int REQUEST_CODE_CALLBACK = 0;
     public static final String EXTRA_KEY_SEARCH_OPTION = "search_option";
     public static final String EXTRA_KEY_POSITION = "position";
 
+    private FragmentPagerAdapter pagerAdapter;
     private CircleSearchOption searchOption;
     private int currentPosition;
+    private ViewPager pager;
+    private View headerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        getActionBar().setTitle("");
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.circlebinder_activity_view_pager);
+
+        headerView = findViewById(R.id.circlebinder_activity_view_pager_header);
 
         final RestoreBundle restoreBundle = new RestoreBundle(getIntent(), savedInstanceState);
         searchOption = restoreBundle.getParcelable(EXTRA_KEY_SEARCH_OPTION);
         currentPosition = restoreBundle.getInt(EXTRA_KEY_POSITION);
 
-        final ViewPager pager = (ViewPager) findViewById(R.id.circlebinder_activity_view_pager);
-        final FragmentPagerAdapter pagerAdapter = new FragmentPagerAdapter(
+        pager = (ViewPager) findViewById(R.id.circlebinder_activity_view_pager);
+        pagerAdapter = new FragmentPagerAdapter(
                 getFragmentManager(),
                 new CircleDetailPagerItem(searchOption)
         );
@@ -54,7 +66,7 @@ public final class CircleDetailPagerActivity extends BaseActivity implements Leg
         pager.setPageMarginDrawable(new ColorDrawable(
                 getResources().getColor(R.color.circlebinder_app_card_container_background)
         ));
-        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        final ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
             @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
             @Override public void onPageScrollStateChanged(int state) {}
 
@@ -71,9 +83,17 @@ public final class CircleDetailPagerActivity extends BaseActivity implements Leg
                 callbackIntent.putExtras(callbackArgs);
                 setResult(REQUEST_CODE_CALLBACK, callbackIntent);
             }
-        });
+        };
+        pager.setOnPageChangeListener(onPageChangeListener);
         pagerAdapter.reload(currentPosition);
         pager.setCurrentItem(currentPosition);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onPageChangeListener.onPageSelected(currentPosition);
+            }
+        }, 1500);
     }
     
     @Override
@@ -85,7 +105,6 @@ public final class CircleDetailPagerActivity extends BaseActivity implements Leg
     @Override
     public void onResume() {
         super.onResume();
-        ViewPager pager = (ViewPager) findViewById(R.id.circlebinder_activity_view_pager);
         if (pager != null) {
             FragmentPagerAdapter pagerAdapter = (FragmentPagerAdapter) pager.getAdapter();
             pagerAdapter.reload(currentPosition);
@@ -98,7 +117,14 @@ public final class CircleDetailPagerActivity extends BaseActivity implements Leg
         outState.putParcelable(EXTRA_KEY_SEARCH_OPTION, searchOption);
         outState.putInt(EXTRA_KEY_POSITION, currentPosition);
     }
-    
+
+    @Override
+    public void onCirclePageChanged(Circle circle) {
+        CircleDetailViewHolder viewHolder = new CircleDetailViewHolder(headerView);
+        viewHolder.getName().setText(circle.getPenName() + "/" + circle.getName());
+        viewHolder.getSpace().setText(circle.getSpace().getName());
+    }
+
     private static class CircleDetailPagerItem implements FragmentPagerItem {
 
         private final CircleCursorConverter cursorCreator;
--- Creation/src/main/java/circlebinder/creation/circle/CircleDetailFragment.java
+++ Creation/src/main/java/circlebinder/creation/circle/CircleDetailFragment.java
@@ -1,6 +1,5 @@
 package circlebinder.creation.circle;
 
-import android.app.ActionBar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,19 +11,18 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 
 import net.ichigotake.common.app.ActionSendFilterActionProvider;
-import net.ichigotake.common.app.ActionViewActivityFactory;
-import net.ichigotake.common.app.ActivityTripper;
 import net.ichigotake.common.app.FragmentFactory;
 import net.ichigotake.common.app.OnPageChangeListener;
+import net.ichigotake.common.app.WebBrowserTripper;
 import net.ichigotake.common.os.RestoreBundle;
+import net.ichigotake.common.widget.OnItemClickEventListener;
 
 import circlebinder.Legacy;
 import circlebinder.common.checklist.ChecklistColor;
+import circlebinder.common.checklist.ChecklistPopupSelector;
 import circlebinder.common.circle.CircleWebContainer;
 import circlebinder.common.event.Circle;
 import circlebinder.common.event.CircleBuilder;
-import circlebinder.common.event.CircleLink;
-import circlebinder.common.view.ReloadWebViewActionProvider;
 import circlebinder.creation.BaseFragment;
 import circlebinder.creation.R;
 import circlebinder.creation.event.CircleTable;
@@ -62,83 +60,83 @@ public final class CircleDetailFragment extends BaseFragment
     private static final String KEY_CIRCLE = "circle";
     private Circle circle;
     private CircleWebContainer webContainer;
-    private ChecklistMenu checklistMenu;
+    private View checklistView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        circle = new RestoreBundle(this, savedInstanceState).getParcelable(KEY_CIRCLE);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        circle = new RestoreBundle(this, savedInstanceState).getParcelable(KEY_CIRCLE);
         View view = inflater.inflate(R.layout.circlebinder_circle_detail, parent, false);
         WebView webView = (WebView)view.findViewById(R.id.circlebinder_circle_detail_web_preview);
         webContainer = new CircleWebContainer(webView);
-        if (circle.getLinks().isEmpty()) {
-            webContainer.load(circle);
-        } else {
-            webContainer.loadUrl(circle.getLinks().get(0));
-        }
-
-        checklistMenu = new ChecklistMenu(getActivity(), this);
-
         return view;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.circle_web, menu);
-        inflater.inflate(R.menu.circle_links, menu);
         MenuItem shareItem = menu.findItem(R.id.circlebinder_menu_circle_web_share);
         shareItem.setActionProvider(
                 new ActionSendFilterActionProvider(getActivity(), webContainer.getCurrentUrl())
         );
-
-        if (!circle.getLinks().isEmpty()) {
-            for (CircleLink link : circle.getLinks().toList()) {
-                MenuItem menuItem = menu.findItem(link.getType().getMenuItemId());
-                menuItem.setVisible(true);
-                menuItem.setActionProvider(
-                        new ReloadWebViewActionProvider(
-                                getActivity(),
-                                webContainer,
-                                link.getUri()
-                        )
-                );
-            }
-        } else {
-            menu.findItem(R.id.circlebinder_menu_circle_links).setVisible(false);
-        }
-        checklistMenu.addMenu(menu, inflater, circle.getChecklistColor());
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.circlebinder_menu_circle_web_open_browser:
-                new ActivityTripper(
-                        getActivity(),
-                        new ActionViewActivityFactory(Uri.parse(webContainer.getCurrentUrl()))
-                ).trip();
+                new WebBrowserTripper(getActivity(), Uri.parse(webContainer.getCurrentUrl())).trip();
                 return true;
         }
-        return checklistMenu.onOptionsItemSelected(item)
-                || super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (circle.getLinks().isEmpty()) {
+            webContainer.load(circle);
+        } else {
+            webContainer.loadUrl(circle.getLinks().get(0));
+        }
+
+        checklistView = getView().findViewById(R.id.circlebinder_circle_detail_checklist);
+        final ChecklistPopupSelector checklistSelector =
+                new ChecklistPopupSelector(getActivity(), checklistView);
+        checklistView.setBackgroundResource(circle.getChecklistColor().getDrawableResource());
+        checklistView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checklistSelector.isShowing()) {
+                    checklistSelector.dismiss();
+                } else {
+                    checklistSelector.show();
+                }
+            }
+        });
+        checklistSelector.setOnItemClickListener(new OnItemClickEventListener<ChecklistColor>() {
+            @Override
+            public void onItemClick(ChecklistColor item) {
+                update(item);
+                checklistSelector.dismiss();
+            }
+        });
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        if (outState == null) {
-            outState = new Bundle();
-        }
-        outState.putParcelable(KEY_CIRCLE, circle);
         super.onSaveInstanceState(outState);
+        outState.putParcelable(KEY_CIRCLE, circle);
     }
 
     @Override
     public void update(ChecklistColor checklistColor) {
+        checklistView.setBackgroundResource(circle.getChecklistColor().getDrawableResource());
         CircleTable.setChecklist(circle, checklistColor);
         circle = new CircleBuilder(circle)
                 .setChecklistColor(checklistColor)
@@ -147,9 +145,7 @@ public final class CircleDetailFragment extends BaseFragment
 
     @Override
     public void active() {
-        if (getActivity() != null && getActivity().getActionBar() != null) {
-            restoreActionBar();
-        }
+        restoreActionBar();
     }
 
     @Override
@@ -158,16 +154,11 @@ public final class CircleDetailFragment extends BaseFragment
     }
 
     private void restoreActionBar() {
-        if (getActivity() != null && getActivity().getActionBar() != null) {
-            ActionBar actionBar =  getActivity().getActionBar();
-            actionBar.setCustomView(R.layout.circlebinder_actionbar_circle_detail);
-            actionBar.setDisplayShowTitleEnabled(false);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowCustomEnabled(true);
-            CircleDetailViewHolder viewHolder = new CircleDetailViewHolder(actionBar.getCustomView());
-            viewHolder.getName().setText(circle.getPenName() + "/" + circle.getName());
-            viewHolder.getSpace().setText(circle.getSpace().getName());
+        if (getActivity() != null) {
             getActivity().invalidateOptionsMenu();
+            if (getActivity() instanceof OnCirclePageChangeListener) {
+                ((OnCirclePageChangeListener)getActivity()).onCirclePageChanged(circle);
+            }
         }
     }
 
