33d18b083b36b1556f381aa4e3f724981c9c2cfd
ichigotake <k.wisiiy@gmail.com>
2014-07-03 08:50:43 +0900

    Implement not navigation drawer layout

--- Creation/src/main/java/circlebinder/creation/app/phone/HomeActivity.java
+++ Creation/src/main/java/circlebinder/creation/app/phone/HomeActivity.java
@@ -1,12 +1,8 @@
 package circlebinder.creation.app.phone;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import net.ichigotake.common.app.ActivityFactory;
 import net.ichigotake.common.app.ActivityTripper;
@@ -14,9 +10,8 @@ import net.ichigotake.common.app.ActivityTripper;
 import circlebinder.Legacy;
 import circlebinder.creation.BaseActivity;
 import circlebinder.creation.R;
-import circlebinder.creation.checklist.ChecklistListFragment;
+import circlebinder.creation.checklist.HomeFragment;
 import circlebinder.creation.initialize.AppStorage;
-import circlebinder.creation.navigation.NavigationDrawerFragment;
 
 /**
  * 通常起動時のファーストビュー
@@ -39,8 +34,6 @@ public final class HomeActivity extends BaseActivity implements Legacy {
         }
     }
 
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,41 +44,11 @@ public final class HomeActivity extends BaseActivity implements Legacy {
             return ;
         }
 
-        setContentView(R.layout.circlebinder_activity_home);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-
-        ChecklistListFragment.tripper(getFragmentManager())
+        setContentView(R.layout.circlebinder_activity_basic);
+        HomeFragment.tripper(getFragmentManager())
                 .setAddBackStack(false)
                 .setLayoutId(R.id.activity_fragment_content)
                 .trip();
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle(R.string.circlebinder_event_name);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (mNavigationDrawerFragment != null && !mNavigationDrawerFragment.isDrawerOpen()) {
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
     }
 
 }
