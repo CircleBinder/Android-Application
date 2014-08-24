14cd9bab39ce9226f6e77ada7b486d2393294105
ichigotake <ichigotake.san@gmail.com>
2014-08-24 12:41:59 +0900

    Fix orientation on CircleDetailPagerActivity

--- app/src/creation/java/circlebinder/creation/circle/CircleDetailViewHolder.java
+++ app/src/creation/java/circlebinder/creation/circle/CircleDetailViewHolder.java
@@ -8,12 +8,20 @@ import circlebinder.R;
 
 public final class CircleDetailViewHolder implements Legacy {
 
+    public static int layoutResource = R.layout.circle_detail_header;
+
+    private final View container;
     private final TextView space;
     private final TextView name;
 
     public CircleDetailViewHolder(View view) {
+        container = view;
         space = (TextView)view.findViewById(R.id.circle_detail_header_space);
         name = (TextView)view.findViewById(R.id.circle_detail_header_name);
+    }
+
+    public View getContainer() {
+        return container;
     }
 
     public TextView getSpace() {
