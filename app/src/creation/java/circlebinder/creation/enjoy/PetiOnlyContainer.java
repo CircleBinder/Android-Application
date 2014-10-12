2ce90f8cd7e9ae5bba13a0770edd49034015b6ad
ichigotake <ichigotake.san@gmail.com>
2014-10-12 14:12:14 +0900

    Fix peti only resources

--- app/src/creation/java/circlebinder/creation/enjoy/PetiOnlyContainer.java
+++ app/src/creation/java/circlebinder/creation/enjoy/PetiOnlyContainer.java
@@ -4,33 +4,36 @@ import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.dmitriy.tarasov.android.intents.IntentUtils;
 import com.squareup.picasso.Picasso;
+
+import net.ichigotake.common.app.OnClickToTrip;
 
 import circlebinder.R;
 
 class PetiOnlyContainer {
 
-    static final int layoutResource = R.layout.enjoy_creation_petionly;
-
-    static void render(View container) {
-        ImageView petiOnly1 = (ImageView) container.findViewById(R.id.event_heads_up_peti_only_1);
-        ImageView petiOnly2 = (ImageView) container.findViewById(R.id.event_heads_up_peti_only_2);
-        new PetiOnlyContainer(petiOnly1, petiOnly2).renderView();
-    }
-
     private final Context context;
-    private final ImageView petiOnly1;
-    private final ImageView petiOnly2;
+    private final View nmnlContainer;
+    private final ImageView nmnlImage;
+    private final View omufesContainer;
+    private final ImageView omufesImage;
 
-    private PetiOnlyContainer(ImageView petiOnly1, ImageView petiOnly2) {
-        this.context = petiOnly1.getContext().getApplicationContext();
-        this.petiOnly1 = petiOnly1;
-        this.petiOnly2 = petiOnly2;
+    PetiOnlyContainer(View container) {
+        this.context = container.getContext().getApplicationContext();
+        this.nmnlContainer = container.findViewById(R.id.enjoy_creation_peti_only_nmnl_container);
+        this.nmnlImage = (ImageView) container.findViewById(R.id.enjoy_creation_peti_only_nmnl_image);
+        this.omufesContainer = container.findViewById(R.id.enjoy_creation_peti_only_omufes_container);
+        this.omufesImage = (ImageView) container.findViewById(R.id.enjoy_creation_peti_only_omufes_image);
     }
 
-    private void renderView() {
-        renderImage(petiOnly1, "http://u.jimdo.com/www47/o/s7c299b1ba1e9f0a7/img/ib658363f5dfafd3a/1402927757/std/image.png");
-        renderImage(petiOnly2, "http://omufes.web.fc2.com/omufes3-a.jpg");
+    void render() {
+        nmnlContainer.setOnClickListener(OnClickToTrip.activityTrip(context,
+                IntentUtils.openLink("http://nmnl.jimdo.com/")));
+        renderImage(nmnlImage, "http://u.jimdo.com/www47/o/s7c299b1ba1e9f0a7/img/i7991934b8e4dc55f/1410948655/std/image.jpg");
+        omufesContainer.setOnClickListener(OnClickToTrip.activityTrip(context,
+                IntentUtils.openLink("http://omufes.web.fc2.com/")));
+        renderImage(omufesImage, "http://omufes.web.fc2.com/omufes3-a.jpg");
     }
 
     private void renderImage(ImageView imageView, String url) {
