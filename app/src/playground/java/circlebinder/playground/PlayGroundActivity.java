6ea5dd091f183b52b2dc57245d39449908e60d78
ichigotake <ichigotake.san@gmail.com>
2014-10-18 12:32:54 +0900

    Create playground flavor

---
+++ app/src/playground/java/circlebinder/playground/PlayGroundActivity.java
@@ -0,0 +1,16 @@
+package circlebinder.playground;
+
+import android.os.Bundle;
+
+import circlebinder.R;
+import circlebinder.common.app.BaseActivity;
+
+public class PlayGroundActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playground_activity);
+    }
+
+}
