840fa8ca6fa1585991581b06f33937770f3f4d10
ichigotake <ichigotake.san@gmail.com>
2014-10-28 08:52:30 +0900

    Fix showduplicate item in home card

--- app/src/main/java/circlebinder/common/card/HomeCardSubscriber.java
+++ app/src/main/java/circlebinder/common/card/HomeCardSubscriber.java
@@ -1,5 +1,6 @@
 package circlebinder.common.card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
@@ -7,15 +8,16 @@ import rx.Subscriber;
 
 final class HomeCardSubscriber implements Observable.OnSubscribe<List<HomeCard>> {
 
-    private final List<HomeCard> cards;
+    private final List<HomeCard> eventCards;
 
     HomeCardSubscriber(List<HomeCard> cards) {
-        this.cards = cards;
+        this.eventCards = cards;
     }
 
     @Override
     public void call(Subscriber<? super List<HomeCard>> subscriber) {
         try {
+            List<HomeCard> cards = new ArrayList<>(eventCards);
             cards.addAll(0, new ChecklistCardCallable().call());
             subscriber.onNext(cards);
             subscriber.onCompleted();
