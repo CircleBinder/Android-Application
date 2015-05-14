a7d47817150b404c35a06a0cbe8481888f874fe6
ichigotake <ichigotake.san@gmail.com>
2015-05-15 02:54:09 +0900

    Wait for idle sync for screenshot

--- app/src/androidTest/java/circlebinder/android/app/activity/CircleBinderActivityTest.java
+++ app/src/androidTest/java/circlebinder/android/app/activity/CircleBinderActivityTest.java
@@ -1,5 +1,6 @@
 package circlebinder.android.app.activity;
 
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.squareup.spoon.Spoon;
@@ -7,6 +8,7 @@ import com.squareup.spoon.Spoon;
 public class CircleBinderActivityTest extends ActivityInstrumentationTestCase2<CircleBinderActivity> {
 
     private CircleBinderActivity activity;
+    private Instrumentation instrumentation;
 
     public CircleBinderActivityTest() {
         super(CircleBinderActivity.class);
@@ -15,9 +17,11 @@ public class CircleBinderActivityTest extends ActivityInstrumentationTestCase2<C
     @Override
     protected void setUp() throws Exception {
         this.activity = getActivity();
+        this.instrumentation = getInstrumentation();
     }
 
     public void test_launch() throws Throwable {
+        instrumentation.waitForIdleSync();
         Spoon.screenshot(activity, "CircleBinderActivity_launch");
     }
 
--- app/src/androidTest/java/circlebinder/android/app/activity/CircleDetailActivityTest.java
+++ app/src/androidTest/java/circlebinder/android/app/activity/CircleDetailActivityTest.java
@@ -1,5 +1,6 @@
 package circlebinder.android.app.activity;
 
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.squareup.spoon.Spoon;
@@ -7,6 +8,7 @@ import com.squareup.spoon.Spoon;
 public class CircleDetailActivityTest extends ActivityInstrumentationTestCase2<CircleDetailActivity> {
 
     private CircleDetailActivity activity;
+    private Instrumentation instrumentation;
 
     public CircleDetailActivityTest() {
         super(CircleDetailActivity.class);
@@ -15,9 +17,11 @@ public class CircleDetailActivityTest extends ActivityInstrumentationTestCase2<C
     @Override
     protected void setUp() throws Exception {
         this.activity = getActivity();
+        this.instrumentation = getInstrumentation();
     }
 
     public void test_launch() throws Throwable {
+        instrumentation.waitForIdleSync();
         Spoon.screenshot(activity, "CircleDetailActivity_launch");
     }
 
